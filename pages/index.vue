f2a280113591b217fca500445fda7262b5118096
Nurjon <nurjon2477@gmail.com>
2024-01-06 00:41:48 +0500

    add ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { tabs } from '~/constants';
+import { tabs, templates } from '~/constants';
 
 useHead({title : 'Jira Software'})
 </script>
@@ -52,5 +52,92 @@ useHead({title : 'Jira Software'})
             </template>
           </UTabs>
         </section>
+
+        <!--  -->
+  		<section>
+  			<h1 class="text-4xl font-medium text-center mt-20">Easier than ever</h1>
+  			<div class="flex items-center justify-between max-w-xl mx-auto mt-4">
+  				<div
+  					class="w-16 h-16 bg-blue-500 dark:bg-blue-800 rounded-full flex items-center justify-center"
+  				>
+  					<Icon name="material-symbols:file-copy" class="text-white text-4xl" />
+  				</div>
+  				<div
+  					class="flex-1 h-2 bg-gradient-to-r from-blue-500 dark:from-blue-800 to-gray-500 dark:to-gray-800"
+  				></div>
+  				<div
+  					class="w-16 h-16 bg-gray-500 dark:bg-gray-800 rounded-full flex items-center justify-center"
+  				>
+  					<Icon
+  						name="material-symbols-light:linked-services"
+  						class="text-4xl"
+  					/>
+  				</div>
+  			</div>
+
+  			<div class="grid grid-cols-2 mt-20">
+  				<div class="bg-blue-500 dark:bg-blue-800 p-8">
+  					<div class="flex flex-col space-y-2 items-center">
+  						<h1 class="text-2xl font-medium">
+  							Templates give you a head start
+  						</h1>
+  						<p class="text-xl">Get started with ready-made templates.</p>
+  					</div>
+
+  					<div v-for="(item, index) in templates" :key="index">
+  						<div class="flex flex-col mt-4">
+  							<div
+  								class="bg-gray-500 dark:bg-gray-800 flex items-center gap-5 h-20"
+  							>
+  								<div
+  									class="flex justify-center items-center bg-blue-500/30 dark:bg-blue-800/30 p-3 h-full"
+  								>
+  									<NuxtImg :src="item.image" class="w-12" />
+  								</div>
+  								<div class="flex flex-col space-y-2">
+  									<h2 class="font-medium text-xl">{{ item.name }}</h2>
+  									<div
+  										class="group flex items-center hover:underline"
+  										role="button"
+  									>
+  										<span>Learn more</span>
+  										<Icon
+  											name="material-symbols:arrow-right-alt-rounded"
+  											class="group-hover:translate-x-2 transition"
+  										/>
+  									</div>
+  								</div>
+  							</div>
+  						</div>
+  					</div>
+
+  					<div
+  						class="py-4 mt-12 rounded-r-full flex flex-col space-y-2 bg-gradient-to-l from-gray-500 dark:from-gray-800 to-blue-500 dark:to-blue-800"
+  					>
+  						<Icon name="bxs:quote-alt-left" class="text-7xl" />
+  						<p class="text-lg pl-2">
+  							You can decide how you are working as a team first and then let
+  							the software work around you."
+  						</p>
+  						<div class="pl-2 opacity-70">
+  							<p class="font-bold">MARIA BONORCHIS</p>
+  							<p class="text-sm">AGILE DELIVERY MANAGER, CANCER RESEARCH UK</p>
+  						</div>
+  					</div>
+  				</div>
+  				<div class="bg-gray-500 dark:bg-gray-800 p-8">
+  					<div class="flex flex-col space-y-6 items-center">
+  						<div class="text-center">
+  							<h1 class="text-2xl font-medium">Then customize as you grow</h1>
+  							<p class="text-xl text-center">
+  								Jira adapts to the way you work, not the other way around. Start
+  								simple, customize as you go.
+  							</p>
+  						</div>
+  						<NuxtImg src="/product.webp" />
+  					</div>
+  				</div>
+  			</div>
+  		</section>
       </main>
 </template>
