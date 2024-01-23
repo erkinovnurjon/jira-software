79bc6ef482d37cefbd59a80d7bc4bfcb3fb66f13
Nurjon <nurjon2477@gmail.com>
2024-01-24 00:28:40 +0500

    documents page ui sidebar navbar

--- layouts/documents.vue
+++ layouts/documents.vue
@@ -2,8 +2,10 @@
      <main>
       <LayoutsNavbar />
       <LayoutsSidebar />
-      <section class="min-h-screen bg-white dark:bg-black">
-
+      <section class="min-h-screen bg-white dark:bg-black pl-72 pt-[10vh]">
+          <div class="p-4">
+               <slot />
+          </div>
       </section>
      </main>
 </template>
--- pages/documents.vue
+++ pages/documents.vue
@@ -1,22 +1,32 @@
 <script setup lang="ts">
-import { ACCOUNT } from '~/libs/appwrite';
-import { useLoadingStore } from '~/store/loading.store';
+import { ACCOUNT } from '~/libs/appwrite'
+import { useAuthStore } from '~/store/auth.store'
+import { useLoadingStore } from '~/store/loading.store'
 
 definePageMeta({ layout: 'documents' })
 useHead({ title: 'Documents | Jira software' })
 
 const router = useRouter()
 const loadingStore = useLoadingStore()
+const authStore = useAuthStore()
 
 onMounted(() => {
 	ACCOUNT.get()
-		.then(() => loadingStore.set(false))
-		.catch(() => router.push('/'))
+		.then(response => {
+			loadingStore.set(false)
+			authStore.set({
+				email: response.email,
+				id: response.$id,
+				name: response.name,
+				status: response.status,
+			})
+		})
+		.catch(() => router.push('/auth'))
 })
 </script>
 
-<template >
-      <div>
-          Documents   
-      </div>
+<template>
+	<div>
+		<UButton>To DO</UButton>
+	</div>
 </template>
