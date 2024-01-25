a3884ba614e0ea4563ee632f0e7dde2192c02f94
Nurjon <nurjon2477@gmail.com>
2024-01-25 22:20:30 +0500

    pages add

---
+++ pages/documents/in-progress.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+definePageMeta({ layout: 'documents' })
+useHead({ title: 'in-progress | Jira software' })
+</script>
+
+<template>in-progress</template>
---
+++ pages/documents/to-do.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+definePageMeta({ layout: 'documents' })
+useHead({ title: 'To do | Jira software' })
+</script>
+
+<template>To do</template>
